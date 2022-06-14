39127c8828493d0c4be167103fc0e3383075951a
nishasen <43262505+nishasen@users.noreply.github.com>
2022-06-14 20:23:57 +0530

    Update NotePage.jsx

--- wiznotes/src/Components/Pages/NotePage.jsx
+++ wiznotes/src/Components/Pages/NotePage.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { NoteContainer, NoteMapping, PageTitle, MapChip, SearchTab } from '..';
+import { NoteContainer, NoteMapping, PageTitle, MapChip, SearchTab, Button } from '..';
 import { useFilter } from '../../Context';
 import './Page.css';
 
 const NotePage = () => {
-  const { notes } = useFilter();
+  const { notes, filterDispatch } = useFilter();
   return (
     <div className="page">
         <SearchTab />
+        <Button text="Show all notes" buttonborder={true} onClick={()=>filterDispatch({type: "CLEAR_FILTER"})}/>
         <NoteContainer />
         <MapChip />
         <PageTitle title="Notes" items={notes?.length} />
